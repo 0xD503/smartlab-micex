964183453a7173becb2d74f3238a1969fb837448
0xD503 <ElectricalBrain@protonmail.com>
2020-08-06 23:19:22 +0300

    Working prototype script
    Signed-off-by: 0xD503 <ElectricalBrain@protonmail.com>

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+import os
+from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-url = r"https://smart-lab.ru/q/shares_fundamental/"
+out_dir = "./out"                                       ##  output path
+url = r"https://smart-lab.ru/q/shares_fundamental/"     ##  raw url
 
 max_PE = 16
 max_PS = 4
@@ -11,13 +14,27 @@ max_ND_EBITDA = 2.5
 #max_EV_EBITDA = 10
 
 
+print("Downloading...")
 tables = pd.read_html(url)  ##  Returns list of tables on page
 market_data = tables[0]
 companies = set(market_data["Название"])
 pe = list(market_data["P/E"])
 #yields.sorted
 
-##  Choose dataframe with P/E, yields
+##  Choose dataframe with price ratios
+print("Creating table...")
+low_P_ratio = market_data
+print("Forming data...")
+low_P_ratio = low_P_ratio[(low_P_ratio["P/E"] < max_PE) & (low_P_ratio["P/E"] > 0)] ##  Price/earnings ratio
+low_P_ratio = low_P_ratio[(low_P_ratio["P/S"] < max_PS) & (low_P_ratio["P/S"] > 0)] ##  Price/sales ratio
+low_P_ratio = low_P_ratio[(low_P_ratio["P/B"] < 4) & (low_P_ratio["P/B"] > 0)]      ##  Price/book value ratio
+low_P_ratio = low_P_ratio[(low_P_ratio["ДД ао, %"] is not None) & (low_P_ratio["ДД ао, %"] != "0.0%")]                              ##  With dividends
+low_P_ratio = low_P_ratio[low_P_ratio["долг/EBITDA"] < max_ND_EBITDA]               ##  With low debd load
+
+print("Sorting data...")
+lowest_P_ratio = low_P_ratio.sort_values(by = ["P/E", "P/S", "P/B", "долг/EBITDA", "EV/EBITDA"], ascending = True, ignore_index = False)
+
+"""
 companies_PE = market_data[["Название", "P/E"]]
 ##  Select companies with (0 < P/E < max_PE)
 low_PE = market_data.loc[(market_data["P/E"] < max_PE) & (market_data["P/E"] > 0), ["Название", "P/E"]]
@@ -34,3 +51,16 @@ low_ND_EBITDA.info()
 #lowest_PE = ???????????????
 #lowest_PS = 
 #lowest_PB = 
+"""
+if not os.path.exists(out_dir):
+    Path(out_dir).mkdir(parents = True, exist_ok = True)
+    print("Out directory was created")
+with open("./out/out.csv", 'w') as f:
+    f.write(lowest_P_ratio.to_csv())
+    print("CSV file writen")
+
+with open("./out/out.xlsx", 'w') as f:
+    lowest_P_ratio.to_excel("./out/out.xlsx")
+    print("Excel file writen")
+
+print("Success")
